afc8a6d291996e4fae25a6cc1a4d030a5c269953
flowTumn <air_bl03@yahoo.co.jp>
2016-06-03 21:26:43 +0900

    add FXInfo.

---
+++ include/fx/data/FXInfo.h
@@ -0,0 +1,17 @@
+﻿#ifndef FXINFO_H_INCLUDE__
+#define FXINFO_H_INCLUDE__
+
+namespace flowTumn {
+	struct FXInfo {
+		double rateCurrent;	// 現値
+		double rateOpen;	// 始値
+		double rateClose;	// 終値
+		double rateHigh;	// 高値
+		double rateLow;		// 低音
+	};
+
+	//現値だけでFXInfoを生成。
+	FXInfo factoryFXInfoCurrentRate(double rateCurrent);
+};
+
+#endif // FXINFO_H_INCLUDE__
---
+++ src/data/FXInfo.cpp
@@ -0,0 +1,7 @@
+﻿#include "fx/data/FXInfo.h"
+
+namespace flowTumn {
+	FXInfo factoryFXInfoCurrentRate(double rateCurrent) {
+		return{ rateCurrent, 0.0, 0.0, 0.0, 0.0 };
+	}
+}
